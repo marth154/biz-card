5549f3078af2ee4a2283edb8ad054f8a2ed35747
marth154 <martin.danvers@gmail.com>
2022-12-04 18:18:34 +0100

    Shwo contact

--- src/components/ListContact.js
+++ src/components/ListContact.js
@@ -1,46 +1,22 @@
+import { Button } from "@mantine/core";
 
 export default function ListContact() {
-  //   const props = await navigator.contacts.getProperties();
-  //   const list = document.querySelector("#contacts");
+  const props = ["name", "email", "tel", "address", "icon"];
+  const opts = { multiple: true };
 
-  //   list.innerHTML = "";
+  async function getContacts() {
+    try {
+      const contacts = await navigator.contacts.select(props, opts);
+      console.log(contacts);
+    } catch (ex) {
+      // Handle any errors here.
+    }
+  }
+  console.log(navigator.contacts);
 
-  //   const showContacts = async () => {
-  //     const contacts = await navigator.contacts.select(props, {
-  //       multiple: true,
-  //     });
-  // const html = contacts.reduce((html, contact) => {
-  //   const names = contact.name.join(", ");
-  //   const emails = contact.email.join(", ");
-  //   const telephone = contact.tel.join(", ");
-
-  //   return `${html}
-  //     <p>
-  //       <span>
-  //         <i class="material-icons">person</i>
-  //         <strong>${names}</strong><br>
-  //       </span>
-  //       <span>
-  //         <i class="material-icons">mail_outline</i>
-  //         ${emails}<br>
-  //       </span>
-  //       <span>
-  //         <i class="material-icons">phone</i>
-  //         ${telephone}</p>
-  //       </span>
-  //     `;
-  // }, ``);
-
-  //     contacts.map((contact) => console.log(contact))
-
-  //   };
-
-//   return <Button onClick={() => showContacts()}>Show contact</Button>;
-    console.log(navigator.contacts);
-
-    return (
-        <>
-            Contact
-        </>
-    )
+  return (
+    <>
+      <Button onClick={() => getContacts()}>Contacts </Button>
+    </>
+  );
 }
